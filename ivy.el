85682ee152f151bda6d929ac8cb24f983ebc92e0
Oleh Krehel <ohwoeowho@gmail.com>
2015-10-14 14:55:02 +0200

    Fix "C-x h" selection and "C-b" bug
    * ivy.el (ivy--exhibit): Add another `constrain-to-field'.
    
    This ensures that the point doesn't cross into the prompt text.
    Previously, there was a bug that pressing "C-b" once more when already
    at the start of the input set `ivy-text' to "", i.e. ignoring the
    minibuffer input.

--- ivy.el
+++ ivy.el
@@ -1454,6 +1454,7 @@ Insert .* between each char."
   "Insert Ivy completions display.
 Should be run via minibuffer `post-command-hook'."
   (when (memq 'ivy--exhibit post-command-hook)
+    (constrain-to-field nil (point-max))
     (setq ivy-text (ivy--input))
     (if (ivy-state-dynamic-collection ivy-last)
         ;; while-no-input would cause annoying
