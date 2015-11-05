abo-abo/swiper

commit 71695df6ea3eb24b52d2a2e1f8587db3db3515a1

"C-j" should not stop completion for a pasted file path
* ivy.el (ivy-alt-done): If you paste a file path, it won't match
  anything in the current directory. Previously, "C-j" would open dired
  for that path. Now, "C-j" will switch to the pasted directory and
  continue completion.

This behavior conforms to `ido-find-file'.

ivy.el
@@ -374,6 +374,9 @@ When ARG is t, exit with current text, ignoring the candidates."
            (ivy-immediate-done))
           ((and ivy--directory
                 (or
+                 (and (file-directory-p ivy-text)
+                      (setq dir (expand-file-name
+                                 ivy-text ivy--directory)))
                  (and
                   (not (string= ivy--current "./"))
                   (cl-plusp ivy--length)

commit b91870eb1c4861ad5ef7700caf5c45ec192d84df

Fix /ssh: and /sudo:: broken in 71695df
* ivy.el (ivy-alt-done): `file-directory-p' errors when given "/ssh:" or
  "/sudo::".

Re #283

ivy.el
@@ -387,7 +387,8 @@ When ARG is t, exit with current text, ignoring the candidates."
                 (or
                  (and
                   (not (equal ivy-text ""))
-                  (file-directory-p ivy-text)
+                  (ignore-errors
+                    (file-directory-p ivy-text))
                   (setq dir (expand-file-name
                              ivy-text ivy--directory)))
                  (and