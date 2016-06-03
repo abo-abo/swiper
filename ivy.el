a566403517d9df547bed5611b0f8c67564b3cd1a
Oleh Krehel <ohwoeowho@gmail.com>
2016-06-03 09:18:35 +0200

    ivy.el (ivy-read): Document all args
    Fixes #533

--- ivy.el
+++ ivy.el
@@ -1284,8 +1284,17 @@ escape it with %%. See also `ivy-count-format'.
 COLLECTION is either a list of strings, a function, an alist, or
 a hash table.
 
+PREDICATE is applied to filter out the COLLECTION immediately.
+This argument is for `completing-read' compat.
+
+When REQUIRE-MATCH is non-nil, only memebers of COLLECTION can be
+selected, i.e. custom text.
+
 If INITIAL-INPUT is not nil, then insert that input in the
 minibuffer initially.
+
+HISTORY is a name of a variable to hold the completion session
+history.
 
 KEYMAP is composed with `ivy-minibuffer-map'.
 
