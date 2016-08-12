8f282a5cc62d2e93b8f261543327406fa8c94143
Oleh Krehel <ohwoeowho@gmail.com>
2016-08-12 13:42:27 +0200

    counsel.el (counsel-git-grep-action): Reveal text in outlines

--- counsel.el
+++ counsel.el
@@ -938,6 +938,7 @@ Describe the selected candidate."
         (goto-char (point-min))
         (forward-line (1- (string-to-number line-number)))
         (re-search-forward (ivy--regex ivy-text t) (line-end-position) t)
+        (swiper--ensure-visible)
         (unless (eq ivy-exit 'done)
           (swiper--cleanup)
           (swiper--add-overlays (ivy--regex ivy-text)))))))
