80db2cc8f1c55708d4bb5a1bf1655019d7c010c5
Oleh Krehel <ohwoeowho@gmail.com>
2016-08-14 13:52:01 +0200

    ivy-test.el (ivy-read-preselect): New test

--- ivy-test.el
+++ ivy-test.el
@@ -408,4 +408,20 @@
   (should (not (ivy-re-match '(("counsel" . nil) ("defun" . t)) "(defun counsel")))
   (should (not (ivy-re-match '(("defun" . t) ("counsel" . nil)) "(defun counsel"))))
 
+(ert-deftest ivy-read-preselect ()
+  (should (equal
+           (ivy-with
+            '(ivy-read "test: "
+              (list "abc" "default" "def")
+              :preselect 1)
+            "RET")
+           "default"))
+  (should (equal
+           (ivy-with
+            '(ivy-read "test: "
+              (list "abc" "default" "def")
+              :preselect "defa")
+            "RET")
+           "default")))
+
 (provide 'ivy-test)
