a77a265e00ad62bf6f73b3d4bbef5016c15741e7
Jonas Bernoulli <jonas@bernoul.li>
2016-12-13 13:11:28 +0100

    Set `outline-regexp' directory local

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((emacs-lisp-mode
-  (indent-tabs-mode . nil)))
+  (indent-tabs-mode . nil)
+  (outline-regexp . ";;\\([;*]+ [^\s\t\n]\\|###autoload\\)\\|(")))
