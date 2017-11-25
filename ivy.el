3dea9acdd8e25ff4ad54dbd4f658978432a67ce3
Tomasz Kołodziejski <tkolodziejski@gmail.com>
2017-11-25 11:57:00 +0000

    Use directory-file-name

--- ivy.el
+++ ivy.el
@@ -1350,8 +1350,7 @@ This string is inserted into the minibuffer."
 Prioritize directories."
   (if (get-text-property 0 'dirp x)
       (if (get-text-property 0 'dirp y)
-          (string< (if (string-suffix-p "/" x) (substring x 0 -1) x)
-                   (if (string-suffix-p "/" y) (substring y 0 -1) y))
+          (string< (directory-file-name x) (directory-file-name y))
         t)
     (if (get-text-property 0 'dirp y)
         nil
