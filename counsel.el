a3420492641dbf9d2a5bf1ccca1529ed1e8dae9a
Oleh Krehel <ohwoeowho@gmail.com>
2017-12-09 14:41:58 +0100

    counsel.el (counsel-minibuffer-history): Add copy-sequence

--- counsel.el
+++ counsel.el
@@ -3366,7 +3366,8 @@ And insert it into the minibuffer.  Useful during `eval-expression'."
   (interactive)
   (let ((enable-recursive-minibuffers t))
     (ivy-read "Reverse-i-search: " (delete-dups
-                                    (symbol-value minibuffer-history-variable))
+                                    (copy-sequence
+                                     (symbol-value minibuffer-history-variable)))
               :action #'insert
               :caller 'counsel-minibuffer-history)))
 (make-obsolete 'counsel-expression-history 'counsel-minibuffer-history "20171011")
