308b1767a9dd253938939a5076497ec5df08edc8
Oleh Krehel <ohwoeowho@gmail.com>
2018-02-02 18:14:29 +0100

    targets/plain.el: Bind some keys

--- targets/plain.el
+++ targets/plain.el
@@ -4,4 +4,6 @@
 (setq enable-recursive-minibuffers t)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-rg)
 (global-set-key (kbd "C-c s") 'isearch-forward-regexp)
