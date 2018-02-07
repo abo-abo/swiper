a08f2a1744e4357a8cfb5a4259690a4a4689b710
Basil L. Contovounesios <contovob@tcd.ie>
2018-02-07 01:19:30 +0000

    Enable sentence-end-double-space in all modes
    .dir-locals.el: Do it.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,10 +3,10 @@
 
 ((nil
   (bug-reference-bug-regexp . "#\\(?2:[[:digit:]]+\\)")
-  (bug-reference-url-format . "https://github.com/abo-abo/swiper/issues/%s"))
+  (bug-reference-url-format . "https://github.com/abo-abo/swiper/issues/%s")
+  (sentence-end-double-space . t))
  (emacs-lisp-mode
   (indent-tabs-mode . nil)
   (outline-regexp . ";;\\([;*]+ [^\s\t\n]\\|###autoload\\)\\|(")
-  (sentence-end-double-space . t)
   ;; (lisp-indent-function . common-lisp-indent-function)
   ))
