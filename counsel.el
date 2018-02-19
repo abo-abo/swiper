5b5dcb4acfcb74c5f4d01e24f613dd5ef2bfb062
Oleh Krehel <ohwoeowho@gmail.com>
2018-02-19 20:13:00 +0100

    counsel.el (counsel--find-file-matcher): Allow use of $ for dirs
    Fixes #1316

--- counsel.el
+++ counsel.el
@@ -1699,7 +1699,10 @@ but the leading dot is a lot faster."
 (defun counsel--find-file-matcher (regexp candidates)
   "Return REGEXP matching CANDIDATES.
 Skip some dotfiles unless `ivy-text' requires them."
-  (let ((res (ivy--re-filter regexp candidates)))
+  (let ((res
+         (cl-remove-if-not
+          (lambda (x) (string-match regexp (directory-file-name x)))
+          candidates)))
     (if (or (null ivy-use-ignore)
             (null counsel-find-file-ignore-regexp)
             (string-match "\\`\\." ivy-text))
