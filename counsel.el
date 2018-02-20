b656f818efd797422886bec455d3cc15a9816c87
Justin Burkett <justin@burkett.cc>
2018-02-19 22:11:05 -0500

    counsel.el (counsel-outline): autoload
    Fixes #1465

--- counsel.el
+++ counsel.el
@@ -3724,6 +3724,7 @@ TREEP is used to expand internal nodes."
   (with-ivy-window
     (goto-char (cdr x))))
 
+;;;###autoload
 (defun counsel-outline ()
   "Jump to outline with completion."
   (interactive)
