2fcde414b1d369f75d937d53b9913cd61ee92462
Sebastian Wålinder <s.walinder@gmail.com>
2018-08-09 22:54:38 +0200

    Added counsel-grep-map, added swiper-avy key to counsel-git-grep
    Fixes #1711

--- counsel.el
+++ counsel.el
@@ -2410,6 +2410,7 @@ It applies no filtering to ivy--all-candidates."
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-l") 'ivy-call-and-recenter)
     (define-key map (kbd "M-q") 'counsel-git-grep-query-replace)
+    (define-key map (kbd "C-'") 'swiper-avy)
     map))
 
 (defcustom counsel-ag-base-command
@@ -2581,6 +2582,13 @@ RG-PROMPT, if non-nil, is passed as `ivy-read' prompt argument."
 (cl-pushnew 'counsel-rg ivy-highlight-grep-commands)
 
 ;;** `counsel-grep'
+(defvar counsel-grep-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-l") 'ivy-call-and-recenter)
+    (define-key map (kbd "M-q") 'swiper-query-replace)
+    (define-key map (kbd "C-'") 'swiper-avy)
+    map))
+
 (defcustom counsel-grep-base-command "grep -E -n -e %s %s"
   "Format string used by `counsel-grep' to build a shell command.
 It should contain two %-sequences (see function `format') to be
@@ -2675,6 +2683,7 @@ When non-nil, INITIAL-INPUT is the initial search pattern."
                                          (line-beginning-position)
                                          (line-end-position)))))
 
+                             :keymap counsel-grep-map
                              :history 'counsel-git-grep-history
                              :update-fn (lambda ()
                                           (counsel-grep-action (ivy-state-current ivy-last)))
