1c61d76dc1a407e5fee926d144a9228b5c46a668
Nathan Moreau <nathan.moreau@m4x.org>
2018-12-20 21:07:28 +0100

    counsel.el (counsel-ag-base-command): use a single command on windows and elsewhere.
    ag --vimgrep seems to yield the same output on ubuntu, cmd and cygwin.

--- counsel.el
+++ counsel.el
@@ -2504,9 +2504,7 @@ It applies no filtering to ivy--all-candidates."
     map))
 
 (defcustom counsel-ag-base-command
-  (if (memq system-type '(ms-dos windows-nt))
-      "ag --vimgrep %s"
-    "ag --nocolor --nogroup %s")
+  "ag --vimgrep %s"
   "Format string to use in `counsel-ag-function' to construct the command.
 The %s will be replaced by optional extra ag arguments followed by the
 regex string."
