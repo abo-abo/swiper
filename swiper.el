8a0ba73945c8f26ec638e2bd81438b61ad7bad77
Iku Iwasa <iku.iwasa@gmail.com>
2019-01-22 23:52:29 +0900

    swiper.el (swiper-font-lock-exclude): Add deadgrep-mode
    Fixes #1902

--- swiper.el
+++ swiper.el
@@ -303,7 +303,8 @@
     matrix-client-mode
     matrix-client-room-list-mode
     sauron-mode
-    w3m-mode)
+    w3m-mode
+    deadgrep-mode)
   "List of major-modes that are incompatible with `font-lock-ensure'.")
 
 (defun swiper-font-lock-ensure-p ()
