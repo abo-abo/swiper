abo-abo/swiper

commit 1439e8dde2b2338e757c0efe24995b7d7f2991f5

counsel.el: Simplify compile build dir collection
(counsel-compile-get-build-directories):
Remove one level of string consing.

counsel.el
@@ -5218,19 +5218,15 @@ The optional BLDDIR is useful for other helpers that have found
 (defun counsel-compile-get-build-directories (&optional dir)
   "Return a list of potential build directories."
   (let* ((srcdir (or dir (funcall counsel-compile-root-function)))
-         (blddir (counsel--find-build-subdir srcdir)))
-    (when blddir
-      (mapcar
-       (lambda (sd)
-         (propertize
-          (concat
-           (propertize "select build in "
-                       'face 'font-lock-warning-face)
-           (propertize sd 'face 'dired-directory))
-          'srcdir srcdir
-          'blddir sd
-          'recursive 't))
-       (counsel--get-build-subdirs blddir)))))
+         (blddir (counsel--find-build-subdir srcdir))
+         (props `(srcdir ,srcdir blddir ,blddir recursive t)))
+    (mapcar (lambda (s)
+              (setq s (concat (propertize "Select build in "
+                                          'face 'font-lock-warning-face)
+                              (propertize s 'face 'dired-directory)))
+              (add-text-properties 0 (length s) props s)
+              s)
+            (and blddir (counsel--get-build-subdirs blddir)))))
 
 ;; No easy way to make directory local, would buffer local make more sense?
 (defun counsel-compile-get-filtered-history (&optional dir)

commit 5113a729bc81ec23daceeb4ce66d8864f50a0a39

counsel.el (counsel-compile): fix breakage to subdir recursion
Commit 1439e8d broke subdir recursion by only passing the directory
containing the subdirs rather than the final subdir. Fix this by
partially reverting the change while keeping the new cleaner
structure.

counsel.el
@@ -5233,13 +5233,14 @@ subdirectories that builds may be invoked in."
   "Return a list of potential build directories."
   (let* ((srcdir (or dir (counsel--compile-root)))
          (blddir (counsel--find-build-subdir srcdir))
-         (props `(srcdir ,srcdir blddir ,blddir recursive t)))
-    (mapcar (lambda (s)
-              (setq s (concat (propertize "Select build in "
-                                          'face 'font-lock-warning-face)
-                              (propertize s 'face 'dired-directory)))
-              (add-text-properties 0 (length s) props s)
-              s)
+         (props `(srcdir ,srcdir recursive t))
+         (fmt (concat (propertize "Select build in "
+                                  'face 'font-lock-warning-face)
+                      (propertize "%s" 'face 'dired-directory))))
+    (mapcar (lambda (subdir)
+              (let ((s (format fmt subdir)))
+                (add-text-properties 0 (length s) `(blddir ,subdir ,@props) s)
+                s))
             (and blddir (counsel--get-build-subdirs blddir)))))
 
 ;; No easy way to make directory local, would buffer local make more sense?