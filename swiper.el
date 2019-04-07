3ab3d5f42edd7106d029c2768ac5cc99ddb7c012
Oleh Krehel <ohwoeowho@gmail.com>
2019-04-07 17:01:19 +0200

    swiper.el (swiper-isearch): Check executing-kbd-macro
    This way, `swiper-isearch' can be used like `isearch' when recording
    macros.
    
    The reason for using `line-beginning-position' is for "C-s M-n" to
    work.

--- swiper.el
+++ swiper.el
@@ -1196,7 +1196,9 @@ come back to the same place as when \"a\" was initially entered.")
   (swiper--init)
   (setq swiper--isearch-point-history
         (list
-         (cons "" (line-beginning-position))))
+         (cons "" (if executing-kbd-macro
+                      (point)
+                    (line-beginning-position)))))
   (let ((ivy-fixed-height-minibuffer t)
         (cursor-in-non-selected-windows nil)
         (swiper-min-highlight 1)
