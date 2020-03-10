45a3609b82713f06c77099f9d816fd2c7e1a2230
Oleh Krehel <ohwoeowho@gmail.com>
2020-03-10 21:12:26 +0100

    counsel.el (counsel-rg-base-command): Add "-M 120" by default
    It's easier for the user to turn this off than to turn it on.
    
    Fixes #2482

--- counsel.el
+++ counsel.el
@@ -3034,8 +3034,8 @@ This uses `counsel-ag' with `counsel-ack-base-command' replacing
 ;;** `counsel-rg'
 (defcustom counsel-rg-base-command
   (if (memq system-type '(ms-dos windows-nt))
-      "rg --with-filename --no-heading --line-number --path-separator / --color never %s ."
-    "rg --with-filename --no-heading --line-number --color never %s")
+      "rg -M 120 --with-filename --no-heading --line-number --color never %s --path-separator /."
+    "rg -M 120 --with-filename --no-heading --line-number --color never %s")
   "Alternative to `counsel-ag-base-command' using ripgrep.
 
 Note: don't use single quotes for the regex."
