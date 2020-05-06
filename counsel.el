abo-abo/swiper

commit cfb395ef07a515e1e805547c29046dc24464f395

counsel.el (counsel--sync-sentinel-on-exit): Simplify
Re #2477

counsel.el
@@ -222,13 +222,12 @@ respectively."
               (if (eq (ivy-alist-setting ivy-index-functions-alist) 'ivy-recompute-index-zero)
                   (ivy-set-index 0)
                 (ivy--recompute-index re ivy--all-candidates))
-            (unless (ivy-set-index
-                     (ivy--preselect-index
-                      (if (> (length re) 0)
-                          cur
-                        (ivy-state-preselect ivy-last))
-                      ivy--all-candidates))
-              (ivy--recompute-index re ivy--all-candidates))))
+            (ivy-set-index
+             (ivy--preselect-index
+              (if (> (length re) 0)
+                  cur
+                (ivy-state-preselect ivy-last))
+              ivy--all-candidates))))
         (setq ivy--old-cands ivy--all-candidates)
         (if ivy--all-candidates
             (ivy--exhibit)

commit ace24753597403d038f1d5d093efa62192cac5c7

counsel.el (counsel--sync-sentinel-on-exit): Fix regression
Commit cfb395ef07a515e1e805547c29046dc24464f395 caused a regression in
`counsel-grep', where `ivy-index-functions-alist' was not honored.

Re #2477

counsel.el
@@ -223,11 +223,14 @@ respectively."
                   (ivy-set-index 0)
                 (ivy--recompute-index re ivy--all-candidates))
             (ivy-set-index
-             (ivy--preselect-index
-              (if (> (length re) 0)
-                  cur
-                (ivy-state-preselect ivy-last))
-              ivy--all-candidates))))
+             (let ((func (ivy-alist-setting ivy-index-functions-alist)))
+               (if func
+                   (funcall func re ivy--all-candidates)
+                 (ivy--preselect-index
+                  (if (> (length re) 0)
+                      cur
+                    (ivy-state-preselect ivy-last))
+                  ivy--all-candidates))))))
         (setq ivy--old-cands ivy--all-candidates)
         (if ivy--all-candidates
             (ivy--exhibit)