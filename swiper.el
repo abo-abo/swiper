71ee4670e8fdc432df8a0b4c6e94acee32a3c9f4
Justin Schell <justin@gudtech.com>
2020-07-21 10:38:57 -0700

    Make C-r behave like C-s; use previous history element for blank input

--- swiper.el
+++ swiper.el
@@ -154,6 +154,14 @@ If the input is empty, select the previous history element instead."
     (ivy-next-line arg)))
 (ivy--no-M-x #'swiper-C-s #'ivy--minibuffer-p)
 
+(defun swiper-C-r (&optional arg)
+  "Move cursor vertically up ARG candidates.
+If the input is empty, select the previous history element instead."
+  (interactive "p")
+  (if (string= ivy-text "")
+      (ivy-previous-history-element 1)
+    (ivy-previous-line arg)))
+
 (defvar swiper-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-s") #'swiper-C-s)
