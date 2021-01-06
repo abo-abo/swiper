a018ee2c9cd3e1701e231aadd0f7828377364b16
Nathoufresh <colinetnathan98@gmail.com>
2021-01-06 11:57:02 +0100

    Fix #2663

--- ivy.el
+++ ivy.el
@@ -2156,7 +2156,7 @@ customizations apply to the current completion session."
                     (ivy-state-initial-input ivy-last)
                     (make-composed-keymap keymap ivy-minibuffer-map)
                     nil
-                    hist)
+                    (copy-tree hist))
                  (error
                   (unless (equal err '(error "Selecting deleted buffer"))
                     (signal (car err) (cdr err))))))
